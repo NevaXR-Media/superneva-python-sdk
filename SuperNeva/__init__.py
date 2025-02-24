fc15b2dfd09a917e551a4971edc1b491f4b59521
Berkay Sargın <4539951+berkaytheunicorn@users.noreply.github.com>
2025-02-24 18:39:35 +0300

    v0.1.2

--- SuperNeva/__init__.py
+++ SuperNeva/__init__.py
@@ -7,5 +7,5 @@ if TYPE_CHECKING:
 
 from SuperNeva import SuperNeva
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["SuperNeva"]
--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from SuperNeva import SuperNeva
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = [
     "SuperNeva",
 ]
